554f828b517f1f0a41712c5199c6370b3aa22a0d
louispersonal <67544146+louispersonal@users.noreply.github.com>
2024-01-17 11:46:13 -0800

    Added explosion test

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -13,15 +13,22 @@ public class Explosion : MonoBehaviour
     [SerializeField]
     float _baseForce;
 
+    [SerializeField]
+    float _maximumRadius;
+
     private void FixedUpdate()
     {
         _circleCollider.radius += _radialAcceleration;
+        if (_circleCollider.radius > _maximumRadius)
+        {
+            Destroy(gameObject);
+        }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         float forceMagnitude = _baseForce / (Mathf.Pow(_circleCollider.radius, 2));
         Vector2 forceDirection = (collision.transform.position - this.transform.position).normalized;
-        collision.rigidbody.AddForce(_baseForce * forceDirection);
+        collision.GetComponent<Rigidbody2D>().AddForce(_baseForce * forceDirection);
     }
 }
